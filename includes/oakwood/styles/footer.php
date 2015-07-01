5cf9ab91f1ca16f495c89044ef8fa57451be2b80
oakwoodgates <funkyoaktree@gmail.com>
2015-07-01 01:39:37 -0400

    forgot one

--- includes/oakwood/styles/footer.php
+++ includes/oakwood/styles/footer.php
@@ -7,3 +7,36 @@ Kirki::add_section( 'footer_styles', array(
     'panel'          => 'main_panel',
 ) );
 
+
+
+/*
+Kirki::add_field( '', array(
+    'type'        => 'radio-image',
+    'setting'     => 'radio_image_demo',
+    'label'       => __( 'Bottom Footer Bar', 'kirki' ),
+    'description' => __( '1 column centered or 2 columns split?', 'kirki' ),
+    'help'        => __( 'This is some extra help text.', 'kirki' ),
+    'section'     => 'footer_styles',
+    'default'     => '',
+    'priority'    => 10,
+    'choices'     => array(
+        'option-1' => admin_url() . '/images/align-left-2x.png',
+        'option-2' => admin_url() . '/images/align-center-2x.png',
+    ),
+) );
+*/
+Kirki::add_field( '', array(
+    'type'        => 'radio',
+    'setting'     => 'radio_demo',
+    'label'       => __( 'Bottom Footer Bar', 'kirki' ),
+    'description' => __( '1 column centered or 2 columns split?', 'kirki' ),
+    'help'        => __( 'This is some extra help text.', 'kirki' ),
+    'section'     => 'footer_styles',
+    'default'     => '1',
+    'priority'    => 10,
+    'choices'     => array(
+        '1' => __( 'Option 1', 'kirki' ),
+        '2' => __( 'Option 2', 'kirki' ),
+    ),
+) );
+
